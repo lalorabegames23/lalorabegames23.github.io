cd09d090d6c3c7ed29f895e59413fb8b802a1e38
lalorabegames23 <74878202+lalorabegames23@users.noreply.github.com>
2020-11-22 16:28:18 -0600

    Update mi-footer.js

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -1,7 +1,7 @@
 class MiFooter extends HTMLElement {
   connectedCallback() {
     this.innerHTML = /* html */
-      `Copyright &copy; Gilberto Pacheco Gallegos`;
+      `Copyright &copy; Ramirez Bello Eduardo`;
   }
 }
 customElements.define("mi-footer", MiFooter);
